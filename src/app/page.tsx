11062ff1726be0d86d660a0aea8150b34ebac90a
youngplq10 <patryk.plq@o2.pl>
2025-06-05 20:06:33 +0200

    Latest changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,8 @@
 
 import Hero from "@/app/modules/Hero"
 import Navbar from "@/app/modules/Navbar"
-import { useEffect } from "react"
 
 const Home = () => {
-
-  useEffect(() => {
-    alert("After first try to interact with server - wait up to 2 minutes to let free-hosted backend service to load up :)")
-  }, [])
-
   return (
     <>
       <Navbar />
